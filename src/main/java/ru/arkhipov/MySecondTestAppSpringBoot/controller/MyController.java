4969d00b962a32551c35f606af98b577622ee347
grisha0008 <bogovgriga@gmail.com>
2023-11-13 09:53:07 +0500

    Initial commit

--- src/main/java/ru/arkhipov/MySecondTestAppSpringBoot/controller/MyController.java
+++ src/main/java/ru/arkhipov/MySecondTestAppSpringBoot/controller/MyController.java
@@ -1,18 +1,34 @@
 package ru.arkhipov.MySecondTestAppSpringBoot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.arkhipov.MySecondTestAppSpringBoot.exception.ValidationFailedException;
 import ru.arkhipov.MySecondTestAppSpringBoot.model.Request;
 import ru.arkhipov.MySecondTestAppSpringBoot.model.Response;
+import ru.arkhipov.MySecondTestAppSpringBoot.service.ValidationService;
+
+import javax.validation.Valid;
+import java.text.SimpleDateFormat;
 
 @RestController
 public class MyController {
 
+    private final ValidationService validationService;
+
+    @Autowired
+    public MyController(ValidationService validationService) {
+        this.validationService = validationService;
+    }
+
     @PostMapping(value = "/feedback")
-    public ResponseEntity<Response> feedback(@RequestBody Request request) {
+    public ResponseEntity<Response> feedback(@Valid @RequestBody Request request, BindingResult bindingResult) {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
         Response response = Response.builder()
                 .uid(request.getUid())
@@ -22,7 +38,21 @@ public class MyController {
                 .errorCode("")
                 .errorMessage("")
                 .build();
-
+            try {
+                validationService.isValid(bindingResult);
+            }
+            catch (ValidationFailedException e) {
+                response.setCode("failed");
+                response.setErrorCode("ValidationException");
+                response.setErrorMessage("Ошибка валидаций");
+                return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+            }
+            catch (Exception e){
+                response.setCode("failed");
+                response.setErrorCode("UnknownException");
+                response.setErrorMessage("Произошла непривиденная ошибка");
+                return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
---
+++ src/main/java/ru/arkhipov/MySecondTestAppSpringBoot/exception/ValidationFailedException.java
@@ -0,0 +1,5 @@
+package ru.arkhipov.MySecondTestAppSpringBoot.exception;
+
+public class ValidationFailedException extends Exception {
+    public ValidationFailedException(String message) { super(message); }
+}
